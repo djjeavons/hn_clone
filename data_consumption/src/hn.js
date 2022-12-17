975c0a08843d79c53f0259355f83713a38060e09
David Jeavons <=>
2022-12-17 12:56:00 +0000

    Implement commander to provide CLI options to control the program.

--- data_consumption/src/hn.js
+++ data_consumption/src/hn.js
@@ -1,17 +1,92 @@
+#!/usr/bin/env node
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const chalk = require("chalk");
+const { Command, Option } = require("commander");
 const api = require("./lib/api");
 const story = require("./lib/dal/story");
 const comment = require("./lib/dal/comment");
 
 const log = console.log;
+const program = new Command();
 
 main();
 
 // Entry point to the application. Allows for async/await
 async function main() {
-  await processTopStories();
+  program
+    .name("Hacker News (HN) Data Consumption")
+    .description(
+      "Gets data via the HN API and stores that data in a Postgresql database"
+    )
+    .version("0.0.1");
+
+  program
+    .command("get")
+    .description(
+      "Gets items (top stories, best stories, new stories etc.) from HN"
+    )
+    .addOption(
+      new Option("-t, --type <type>", "Item type").choices([
+        "top",
+        "best",
+        "new",
+        "ask",
+        "show",
+        "ALL",
+      ])
+    )
+    .addOption(
+      new Option(
+        "-l, --latest",
+        "Gets items newer than the max item identifier stored in the database. If no type passed then will assume top stories"
+      )
+    );
+
+  program.command("refresh <itemid>").description("Refreshes the item passed");
+
+  program.parse();
+
+  if (program.args.length > 0) {
+    switch (program.args[0]) {
+      case "refresh":
+        if (isNaN(program.args[1])) {
+          log(
+            chalk.redBright("Error: Item Id to be refreshed must be a number")
+          );
+        } else {
+          const itemToRefresh = parseInt(program.args[1]);
+          // Do the refreshing call
+          console.log(`Refresh the item: ${itemToRefresh}`);
+        }
+        break;
+
+      case "get":
+        // Ensure we have at least one flag (-t, -l)
+        if (program.args.length === 1) {
+          log(
+            chalk.redBright(
+              "Error: At least one argument must be supplied to get"
+            )
+          );
+        }
+
+        let type = "top";
+        let latest = false;
+
+        if (program.args.includes("-t")) {
+          type = program.args[program.args.indexOf("-t") + 1];
+        }
+
+        if (program.args.includes("-l")) {
+          latest = true;
+        }
+
+        // Do the get or latest get here
+        console.log(`Get latest: ${latest} for type: ${type}`);
+        break;
+    }
+  }
 }
 
 async function processTopStories() {
@@ -92,3 +167,5 @@ async function processComments(parentType, itemData) {
     }
   }
 }
+
+async function configureProgram() {}
